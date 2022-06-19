cee18edf5be2009da086386c7b7960e3dae527d7
Jason Docton <Jdocton@gmail.com>
2022-06-19 16:14:29 -0700

    RAD Starter Ready

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
+  experimental: { optimizeCss: true, legacyBrowsers: false, browsersListForSwc: true },
 }
 
 module.exports = nextConfig

e22dbac70cb04aa4076ef0ba0685a421b3e97b09
Jason Docton <Jdocton@gmail.com>
2022-07-01 10:04:03 -0700

    Updated to Next 12.2, prettier using tabs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen align-middle">
-      <h1 className="text-4xl font-extrabold">RAD Starter</h1>
-    </div>
-  )
+	return (
+		<div className="flex flex-col items-center justify-center h-screen align-middle">
+			<h1 className="text-4xl font-extrabold">RAD Starter</h1>
+		</div>
+	)
 }
 
 export default Home
